19bfddfef581fd558c5707a36ee6efe9203ae9ac
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-02 14:12:36 -0300

    file public

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'./views'))
 
+app.use(express.static('public'));
+
 app.use('/',routers())
 
 app.listen(process.env.PORT, () => {
