38a40fcae9159c6ee40709d45069428875b94f9c
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-02 19:17:07 -0300

    install express-ejs-layout

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const routers = require('./routers')
+const expressLayout = require('express-ejs-layouts');
 require('dotenv').config({path: 'variables.env'});
 
 const app = express();
 
+app.use(expressLayout);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'./views'))
 
