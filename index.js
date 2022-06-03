4c16e0a9f6cea017bc2d1df2838758d7cee031ff
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-03 10:31:50 -0300

    install Sequelize

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const express = require('express');
 const path = require('path');
 const routers = require('./routers')
 const expressLayout = require('express-ejs-layouts');
-require('dotenv').config({path: 'variables.env'});
+
+const db = require('./config/db');
+db.sync().then(() => console.log('DB conectada')).catch((error) => console.log(error));
+
+require('dotenv').config({ path: 'variables.env' });
 
 const app = express();
 
 app.use(expressLayout);
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'./views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
 
 app.use(express.static('public'));
 
 // TODO: Variables globales
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     const fecha = new Date();
     res.locals.year = fecha.getFullYear();
     next();
 })
-app.use('/',routers())
+app.use('/', routers())
 
 app.listen(process.env.PORT, () => {
-    console.log('Server running... in port '+ process.env.PORT);
+    console.log('Server running... in port ' + process.env.PORT);
 })
