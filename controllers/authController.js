e63769827135462203990e2fe19e110964363082
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-04 11:41:14 -0300

    method authenticate

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,11 @@ exports.logIn = passport.authenticate('local', {
     failureFlash: true,
     badRequestMessage: 'Ambos campos son obligatorios'
 })
+
+exports.userAutenticate = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    return res.redirect('/user/log-in');
+}
