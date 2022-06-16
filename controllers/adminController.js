e01da681d38252e7988e511fc7e59e725010c108
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-16 13:43:41 -0300

    groups panel admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,12 @@
-exports.panel = (req, res) => {
+const Groups = require('../models/Groups');
+
+exports.panel = async (req, res) => {
+    //const groups = await Groups.findAll({ where: { userId: req.user.id } });
+
+    const groups = await Groups.findAll({ where: { userId: 1 } });
+    
     res.render('./user/admin/panel', {
-        nombrePagina: 'Panel de administracion'
+        nombrePagina: 'Panel de administracion',
+        groups
     })
 }
